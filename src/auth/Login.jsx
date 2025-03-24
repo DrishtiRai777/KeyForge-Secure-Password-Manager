fdc0ed44c7d7ff692da4e343df1dbbc4f24d15e5
Drishti Rai <drishtirai345@gmail.com>
2025-03-24 17:14:02 +0530

    cookies Problem fixed

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -13,7 +13,8 @@ const Login = () => {
     const response = await fetch("http://localhost:3000/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password })
+      body: JSON.stringify({ email, password }),
+      credentials: "include",
     })
 
     const data = await response.json();
